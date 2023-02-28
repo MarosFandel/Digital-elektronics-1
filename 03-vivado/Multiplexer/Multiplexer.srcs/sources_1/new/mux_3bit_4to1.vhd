9e12f98395ade68f6005e55ac8c53ca5c159481b
MarosFandel <124789239+MarosFandel@users.noreply.github.com>
2023-02-28 16:56:57 +0100

    Update mux_3bit_4to1.vhd

--- 03-vivado/Multiplexer/Multiplexer.srcs/sources_1/new/mux_3bit_4to1.vhd
+++ 03-vivado/Multiplexer/Multiplexer.srcs/sources_1/new/mux_3bit_4to1.vhd
@@ -1,52 +1,22 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 02/22/2023 02:27:37 PM
--- Design Name: 
--- Module Name: mux_3bit_4to1 - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity mux_3bit_4to1 is
-  Port (
-    a_i           : in    std_logic_vector(3 downto 0); --! Input data B[3:0]
-    b_i           : in    std_logic_vector(3 downto 0); --! Input data A[3:0]
-    c_i           : out   std_logic;                    --! Output is `1` if B>A
-    d_i           : out   std_logic;                    --! Output is `1` if B=A
-    y_o           : out   std_logic                     --! Output is `1` if B<A );
-end mux_3bit_4to1;
+    port(
+        sel_i         : in  std_logic_vector(2 - 1 downto 0);
+        a_i           : in  std_logic_vector(3 - 1 downto 0);
+        b_i           : in  std_logic_vector(3 - 1 downto 0);
+        c_i           : in  std_logic_vector(3 - 1 downto 0);
+        d_i           : in  std_logic_vector(3 - 1 downto 0);
+        y_o           : out std_logic_vector(3 - 1 downto 0)
+    );
+end entity mux_3bit_4to1;
 
-architecture Behavioral of mux_3bit_4to1 is 
-with addr_i select
-    y_o <= a_i when "000",  -- If addr_i = "000" then y_o = a_i
-           b_i when "001",
-           c_i when "010",
-           d_i when others; -- All other combinations
+
+architecture Behavioral of mux_3bit_4to1 is
 begin
-
-
-end Behavioral;
+    y_o <= a_i when (sel_i = "00") else
+           b_i when (sel_i = "01") else
+           c_i when (sel_i = "10") else
+           d_i;    
+end architecture Behavioral;
